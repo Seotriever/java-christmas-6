92c18732fe3d0e23f76f9607f9d2753b39eb2cfc
Seotriever <skb0218@naver.com>
2023-11-26 18:13:56 +0900

    날짜입력 메소드(예외처리 포함)

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import static christmas.InputView.readDate;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+//        readDate();
+        System.out.println(readDate());
+
     }
 }
