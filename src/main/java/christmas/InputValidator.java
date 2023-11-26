22b4ac93c2459164fb6a3cfc6734123facbb80f5
Seotriever <skb0218@naver.com>
2023-11-26 18:57:10 +0900

    날짜 입력 메소드 객체화

--- src/main/java/christmas/InputValidator.java
+++ src/main/java/christmas/InputValidator.java
@@ -1,2 +1,28 @@
-package christmas;public class InputValidator {
+package christmas;
+
+class InputValidator {
+    public static int tryParseIntDate(String input) {
+        int intDate;
+        try {
+            intDate = Integer.parseInt(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 숫자만 입력해주세요.");
+            throw  new IllegalArgumentException();
+        }
+        return intDate;
+    }
+    public static boolean validateDate(String input) {
+        try {
+            if (tryParseIntDate(input) < 1 || tryParseIntDate(input) > 31) {
+                System.out.println("[ERROR] 유효한 날짜의 숫자만 입력해주세요.");
+                throw new IllegalArgumentException();
+            }
+            tryParseIntDate(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            return false;
+        }
+        return true;
+
+    }
 }
--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.HashMap;
 
+import static christmas.InputValidator.tryParseIntDate;
+import static christmas.InputValidator.validateDate;
+
 public class InputView {
     public static int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
@@ -14,28 +17,6 @@ public class InputView {
         return tryParseIntDate(input);
     }
 
-    public static int tryParseIntDate(String input) {
-        int intDate;
-        try {
-            intDate = Integer.parseInt(input);
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 숫자만 입력해주세요.");
-            throw  new IllegalArgumentException();
-        }
-        return intDate;
-    }
-    public static boolean validateDate(String input) {
-        try {
-            if (tryParseIntDate(input) < 1 || tryParseIntDate(input) > 31) {
-                System.out.println("[ERROR] 유효한 날짜의 숫자만 입력해주세요.");
-                throw new IllegalArgumentException();
-            }
-            tryParseIntDate(input);
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-            return false;
-        }
-        return true;
 
-    }
+
 }
